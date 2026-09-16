bba756520c6fb0615b08ed87f7231e21fc7a198f
vivekhardeniya2550 <vivekhardeniya2550@gmail.com>
2026-09-16 11:54:02 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.41%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,5 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        int a=Arrays.binarySearch(nums, target);
+        return a<0? Math.abs(a)-1:a;        
     }
 }
